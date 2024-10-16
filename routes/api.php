4d02a49d400cd32f03a078cc0521c62561f73cfa
golden <goldenazubuike@gmail.com>
2024-10-16 14:07:54 +0100

    Docker file

--- routes/api.php
+++ routes/api.php
@@ -4,5 +4,6 @@ use App\Controllers\UserController;
 
 // $config = require "../config.php";
 
+$router->get("/", [UserController::class, "index"]);
 $router->get("/users", [UserController::class, 'index']);
 $router->get("/users/{user}", [UserController::class, 'show']);
